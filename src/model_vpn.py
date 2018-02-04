Masking changes, fixes
Should work properly now for training. Had an issue with the VPN learning an identity mapping but that was because I concatenating the frame last instead of first, so masking didn't work. Should still implement proper generation but getting pretty close. Read the paper on speeding up PixelCNN++ generation, realizing that for reasonably ok speeds that might be a necessity. (Generating 16 32x32 frames takes like 11 minutes on K40, they achieve 1-2 orders of magnitude speedup depending on batch size.)

src/model_vpn.py
@@ -13,7 +13,7 @@ class VPN(nn.Module):
                  enc_kernel_size = 3,
                  lstm_layers = 1,
                  use_lstm_peepholes=True,
-		 mask = True):
+                 mask = True):
         """
         Video Pixel Network model
         """
@@ -27,7 +27,8 @@ class VPN(nn.Module):
 
         self.decoder = Decoder(n_rmb=n_rmb_decoder, input_channels=c, image_channels=img_channels,
                       n_context=n_context, output_channels=n_pixvals,
-                      internal_channels=c//2, kernel_size=3)
+                      internal_channels=c//2, kernel_size=3,
+                      mask = mask)
 
     def forward(self, inputs_var, targets=None, n_predict=None):
 
@@ -55,13 +56,13 @@ class VPN(nn.Module):
             # Forward pass of decoder with last context (encoder output) and
             # no target img to condition on (all is generated)
             # NOTE: choosing channel 2
-            preds.append(self.decoder(context[:,-1:], mask=self.mask))
+            preds.append(self.decoder(context[:,-1:]))
 
             # Feed output of decoder to encoder. Do one forward pass for encoder.
             # Pass context to decoder. Do one forward pass for decoder. Repeat this `n_predict` times.
             for i_p in range(n_predict-1):
                 context, lstm_state = self.encoder(preds[-1], lstm_state=lstm_state)
-                preds.append(self.decoder(context, mask=self.mask))
+                preds.append(self.decoder(context))
 
             # Output outputs of decoder layer
             return torch.cat(preds, dim=1)

src/video_pixel_network_pytorch/layers.py
@@ -94,7 +94,7 @@ class ResidualMultiplicativeBlock(nn.Module):
             integrate_frame_channels (int): Number of channels that we need to put on top of layer stack after input
                                             convolution
             mask (bool): Use masked convolutions
-            mu_class: MultiplicativeUnit class. Used as kwa        self.n_rg so we can use other classes if we want.
+            mu_class: MultiplicativeUnit class, other compatible classes can be used instead
         """
         super(ResidualMultiplicativeBlock, self).__init__()
 
@@ -122,7 +122,7 @@ class ResidualMultiplicativeBlock(nn.Module):
         """
         Mask MU's. We have all information needed from the arguments in the constructor
         """
-        # First mask input convolution, if needed
+        # First mask input convolution, if we're not first block
         if not self.integrate_frame_channels:
             mask(self.conv_input.weight, self.image_channels, self.n_context, mask_center_pixel_current=False,
                  input_repeats=True)
@@ -135,8 +135,9 @@ class ResidualMultiplicativeBlock(nn.Module):
             mask(weight, self.image_channels, self.n_context, mask_center_pixel_current=first,
                  input_repeats=not first, n_chunks=n_chunks)
 
+        # TODO: Multichannel support. Need to give division of output channel quantization somewhere
         mask(self.conv_output.weight, self.image_channels, self.n_context, mask_center_pixel_current=False,
-             input_repeats=True, n_logits_per_channel=[self.output_channels])
+             input_repeats=True, n_logits_per_channel=[self.output_channels] if last else None)
 
     def forward(self, h, frame=None, pixel=None):
         """
@@ -161,15 +162,22 @@ class ResidualMultiplicativeBlock(nn.Module):
             return self._forward(h, frame)
 
     def _forward_pixel(self, h, frame, pixel):
+        """
+        Calculates only one pixel of one channel. pixel = (channel, h, w)
+        """
+        i_channel = pixel[0]
+
+        # TODO: Support multiple channels
         padding = self.dilation * (self.kernel_size - 1) // 2
         # TODO: Move this repetitive padding, Decoder.forward() maybe
         # Pad with 0's, padding tuple ordered (left, right, top, bottom)
         x = F.pad(h, [padding] * 4)
         # Take just the patch size necessary for our kernel size and dilation
         half_width = ((self.kernel_size - 1) * self.dilation + 1) // 2
+
         # Add 1 to h_end as end is non-inclusive
-        h_start, h_end = pixel[0] + padding - half_width, pixel[0] + padding + half_width + 1
-        w_start, w_end = pixel[1] + padding - half_width, pixel[1] + padding + half_width + 1
+        h_start, h_end = pixel[1] + padding - half_width, pixel[1] + padding + half_width + 1
+        w_start, w_end = pixel[2] + padding - half_width, pixel[2] + padding + half_width + 1
         # Slice out square with the right size
         x = x[:, :, h_start:h_end, w_start:w_end]
         # Run through 1x1 conv
@@ -180,7 +188,8 @@ class ResidualMultiplicativeBlock(nn.Module):
             frame = F.pad(frame, [padding] * 4)
             frame = frame[:, :, h_start:h_end, w_start:w_end]
             # Concatenate in channel dimension
-            x = torch.cat((x, frame), dim=1)
+            # Make sure to place frame first as that is how we mask
+            x = torch.cat((frame, x), dim=1)
 
         for mu in self.mus:
             # nopad helps in calculating only the center pixel
@@ -192,14 +201,15 @@ class ResidualMultiplicativeBlock(nn.Module):
         if self.additive_skip:
             h_pad = F.pad(h, [padding] * 4)
             x = h_pad[:, :, h_start:h_end, w_start:w_end] + x
-        # Slice to retain dims
-        return x[:, :, half_width:half_width + 1, half_width:half_width + 1]
+        # Shape (b, n_logits)
+        return x[:, :, half_width, half_width]
 
     def _forward(self, h, frame):
         x = self.conv_input(h)
 
         if self.integrate_frame_channels:
-            x = torch.cat((x, frame), dim=1)
+            # Place frame first for masking
+            x = torch.cat((frame, x), dim=1)
 
         for mu in self.mus:
             x = mu(x)

src/video_pixel_network_pytorch/vpn.py
@@ -100,7 +100,7 @@ class Encoder(nn.Module):
 
 class Decoder(nn.Module):
     def __init__(self, n_rmb, input_channels, output_channels, internal_channels, image_channels=None, n_context=None,
-                 kernel_size=3):
+                 kernel_size=3, mask=False):
         """
         Video Pixel Network decoder.
 
@@ -116,12 +116,13 @@ class Decoder(nn.Module):
         """
         super(Decoder, self).__init__()
 
+        self.masking = mask
         self.image_channels = image_channels
 
-        # With current implementation we're creating first, middle and last blocks at least.
-        # Supporting n_rmb < 3 is trivial but would need implementation and is probably not effective anyway.
-        if n_rmb < 3:
-            raise NotImplementedError("Fewer RMBs than 3 are not supported.")
+        # With current implementation we're creating first and last blocks at least.
+        # Supporting n_rmb < 2 is trivial but would need implementation and is probably not effective anyway.
+        if n_rmb < 2:
+            raise NotImplementedError("Fewer RMBs than 2 are not supported.")
         if image_channels > 1:
             raise NotImplementedError("More than 1 image channel is not implemented yet.")
 
@@ -136,7 +137,7 @@ class Decoder(nn.Module):
             n_mu=2,
             kernel_size=kernel_size,
             dilation=1,
-            integrate_frame_channels=image_channels or 0,
+            integrate_frame_channels=image_channels if mask else 0,
             additive_skip=True)])
 
         # Make all RMBs except first and last
@@ -171,7 +172,7 @@ class Decoder(nn.Module):
         for i, rmb in enumerate(self.rmbs):
             rmb.mask(last = i==(len(self.rmbs)-1))
 
-    def forward(self, inputs, targets=None, mask=False, argmax=False):
+    def forward(self, inputs, targets=None, argmax=False):
         """
         Don't call this method but __call__ the class.
 
@@ -186,30 +187,34 @@ class Decoder(nn.Module):
             targets: Targets used as context for masked convolutions
         """
         if self.training:
-            if targets is None:
-                raise AssertionError("self.training=True and targets=None, please supply the targets to train on")
+            if self.masking and targets is None:
+                raise AssertionError("In training mode, self.mask=True and targets=None, please supply the targets to train on")
             return self._forward_train(inputs, targets)
 
         else:
-            if mask:
-                return self._forward_inference_mask(inputs, argmax=argmax)
+            if self.masking:
+                return self._forward_inference_autoregressive(inputs, argmax=argmax)
             else:
-                return self._forward_inference_nomask(inputs, argmax=argmax)
+                return self._forward_inference_vanilla(inputs, argmax=argmax)
 
     def _forward_train(self, inputs, targets):
+        "Training directly on one-hot targets"
         logits = []
         for i_timestep in range(inputs.size(1)):
             x = inputs[:, i_timestep]
-            # Calc all rmbs
-            for rmb in self.rmbs:
-                x = rmb(x)
+            # First RMB needs current context
+            if targets is not None:
+                x = self.rmbs[0](x, frame=targets[:, i_timestep])
+
+            for i_rmb in range(1 if targets is not None else 0, len(self.rmbs)):
+                x = self.rmbs[i_rmb](x)
             # Don't use softmax as we'll use it with loss func for numerical stability
             logits.append(x)
 
         # Add timestep dim
         return torch.stack(logits, dim=1)
 
-    def _forward_inference_nomask(self, inputs, argmax=False):
+    def _forward_inference_vanilla(self, inputs, argmax=False):
         "Inference without conditioning on generated output"
         logits = []
         for i_timestep in range(inputs.size(1)):
@@ -242,10 +247,10 @@ class Decoder(nn.Module):
         # Put channel axis back in place (b,t,c,h,w)
         pix_vals = mult.permute(0,1,4,2,3)
 
-        return pix_vals
+        # Make sure to cast to proper type
+        return pix_vals.type_as(inputs)
 
-
-    def _forward_inference_mask(self, inputs, argmax=False):
+    def _forward_inference_autoregressive(self, inputs, argmax=False):
         "Pixel-by-pixel inference, conditioning on current output"
 
         # Inputs have same b,t,h,w as predictions
@@ -253,76 +258,27 @@ class Decoder(nn.Module):
 
         preds = Variable(torch.zeros(b, t, self.image_channels, h, w))
 
-        # Put on CUDA if we're using it
         if inputs.data.is_cuda:
             preds = preds.cuda()
-# TODO: REWORK THIS SHIT
 
-        logits = []
         for i_timestep in range(inputs.size(1)):
-            x = inputs[:, i_timestep]
-            # Calc all rmbs
-            for rmb in self.rmbs:
-                x = rmb(x)
-            # Don't use softmax as we'll use it with loss func for numerical stability
-            logits.append(x)
+            history = inputs[:, i_timestep]
 
-        # Add timestep dim
-        logits = torch.stack(logits, dim=1)
+            for i_h in range(h):
+                for i_w in range(w):
+                    for i_c in range(self.image_channels):
+                        # TODO: Set up patch that we're predicting?
 
-        soft = F.softmax(logits, dim=2)
+                        # TODO: Run through first RMB. Then all others
+                        for rmb in self.rmbs:
+                            # TODO: Multichannel
+                            pix_logits = rmb(history, frame=preds, pixel=(i_c, i_h, i_w))
+                        # (b, c)
+                        pix_soft = F.softmax(pix_logits, dim=1)
+                        # (b, c)
+                        pix_sampled = torch.multinomial(pix_soft, 1)
+                        # (b, n_samples=1)
 
-        # TODO: Support multi-channel images, where we have to chunk the logits
+                        preds[:, i_c, i_h, i_w] = pix_sampled[:, 0]
 
-        # Put channel axis last (b,t,h,w,c)
-        mult = soft.permute(0,1,3,4,2)
-        size = list(mult.size())
-        # Need contiguous array for .view()
-        mult = mult.contiguous()
-        # Flatten batch, timestep, h, w dimensions so we have list of discrete prob dists
-        mult = mult.view(-1, mult.size(-1))
-        # Then take one sample for each
-        mult = torch.multinomial(mult, 1)
-
-        size[-1] = 1
-        mult = mult.view(*size)
-        # Put channel axis back in place (b,t,c,h,w)
-        pix_vals = mult.permute(0,1,4,2,3)
-
-        return pix_vals
-
-        # # Inputs have same b,t,h,w as predictions
-        # b, t, c, h, w = inputs.size()
-        #
-        # # Only 1 channel currently supported
-        # preds = Variable(torch.zeros(b, t, 1, h, w))
-        # i_channel = 0
-        # # Put on CUDA if we're using it
-        # if inputs.data.is_cuda:
-        #     preds = preds.cuda()
-        #
-        # # Construct output img then cycle over all pixels here
-        # # Sample from logit distribution for the one pixel,
-        # # or choose largest (argmax) if argmax=True
-        # for i_timestep in range(inputs.size(1)):
-        #
-        #     x = inputs[:, i_timestep]
-        #
-        #     for i_h in range(inputs.size(-2)):
-        #         for i_w in range(inputs.size(-1)):
-        #
-        #             # Calc all rmbs
-        #             for rmb in self.rmbs:
-        #                 # Calculates just one pixel at a time
-        #                 pix = rmb(x, frame=preds[:,i_timestep], pixel=(i_h,i_w))
-        #                 # Choose pixel value
-        #                 if argmax:
-        #                     # Over axis of discrete prob dist
-        #                     # Pixel values are index of highest val
-        #                     _, pix_vals = torch.topk(pix[:,:,0,0], 1, dim=1)
-        #                 else:
-        #                     soft = F.softmax(pix[:, :, 0, 0], dim=1)
-        #                     pix_vals = torch.multinomial(soft, 1)
-        #                 preds[:, i_timestep, i_channel, i_h, i_w] = pix_vals
-        #
-        # return preds
+        return preds